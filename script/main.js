36e0ac102ed5a746763bae94baef729d3e835420
hjpark625 <97019802+hjpark625@users.noreply.github.com>
2022-05-11 17:08:58 +0900

    3차 수정
    1. page_style.css 내 body, #container내 overflow 삭제 => TOP버튼 최상위로 이동하는 문제 해결
    2. CSS 정리 후 스타일 재정비
    3. overflow삭제 후 main.js 내 주간야간 모드 재정비
      1) contact_logo i 부분도 주간야간 모드 적용되도록 정비완료

--- script/main.js
+++ script/main.js
@@ -1,10 +1,11 @@
+/* 페이지 내 이메일 출력 함수 */
 function showMessage() {
   document.getElementById("email").innerHTML = "zero950@gmail.com";
 }
 
 document.getElementById("email").addEventListener("click", showMessage);
 
-// 주야 모드 버튼 애니메이션 동작
+/* 주야 모드 버튼 애니메이션 동작 */
 const swithBox = document.querySelector(".night_day");
 
 document.querySelector("input").addEventListener("change", (e) => {
@@ -16,39 +17,71 @@ document.querySelector("input").addEventListener("change", (e) => {
   }
 });
 
-// 주야 모드 배경전환 구현
-let Links = {
-  setColor: function (color) {
-    let alist = document.querySelectorAll("li");
-    let i = 0;
-    while (i < alist.length) {
-      console.log(alist[i]);
-      alist[i].style.color = color;
-      i++;
-    }
-  },
-};
-
+/* 주야 모드 배경전환 구현 */
 let Back = {
   setColor: function (color) {
-    document.querySelector("#container").style.color = color;
+    let sections = document.querySelectorAll(".profile");
+    for (let i = 0; i < sections.length; i++) {
+      let item = sections.item(i);
+      item.style.color = color;
+    }
+    // contact_logo i에 대한 색변경
+    let logo = document.querySelectorAll(".contact_logo i");
+    for (let i = 0; i < logo.length; i++) {
+      let item = logo.item(i);
+      item.style.color = color;
+    }
+    // document.querySelectorAll(".profile").style.color = color;
   },
   setBackgroundColor: function (color) {
-    document.querySelector("#container").style.backgroundColor = color;
+    let sections = document.querySelectorAll(".profile");
+    for (let i = 0; i < sections.length; i++) {
+      let item = sections.item(i);
+      item.style.backgroundColor = color;
+    }
+    // document.querySelectorAll(".profile").style.backgroundColor = color;
   },
 };
 
+/* 주야모드 실행함수 */
 function nightDayHandler(self) {
-  let target = document.querySelector("body");
+  // let target = document.querySelector("body");
   if (self.value === "night") {
     Back.setBackgroundColor("#262e28");
     Back.setColor("#8fc2ae");
     self.value = "day";
-    Links.setColor("#8fc2ae");
   } else {
     Back.setBackgroundColor("#f7fcf8");
     Back.setColor("#6d6e78");
     self.value = "night";
-    Links.setColor("#6d6e78");
   }
+}
+
+/* Back To Top 버튼 */
+document.addEventListener("scroll", handleScroll);
+
+let scrollToTopBtn = document.querySelector(".top_button");
+// 스크롤 했을 때 일정구간에서 버튼의 등장여부 결정
+function handleScroll() {
+  let scrollableHeight =
+    document.documentElement.scrollHeight -
+    document.documentElement.clientHeight;
+  let GOLDEN_RATIO = 0.5;
+
+  if (document.documentElement.scrollTop / scrollableHeight > GOLDEN_RATIO) {
+    // 버튼을 보여주기
+    scrollToTopBtn.style.display = "block";
+  } else {
+    // 버튼을 숨기기
+    scrollToTopBtn.style.display = "none";
+  }
+}
+
+scrollToTopBtn.addEventListener("click", scrollToTop);
+
+function scrollToTop() {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
 }
